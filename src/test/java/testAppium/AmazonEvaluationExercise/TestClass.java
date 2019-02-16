73f6e1082b933849edbe975a8510769b3fa28e26
Shashank Sharma <shashanksharma.er104@gmail.com>
2019-02-16 21:29:12 +0530

    Changes made in the package

--- src/test/java/testAppium/AmazonEvaluationExercise/TestClass.java
+++ src/test/java/testAppium/AmazonEvaluationExercise/TestClass.java
@@ -1,62 +1,23 @@
 package testAppium.AmazonEvaluationExercise;
 
+import org.testng.annotations.*;
 import static io.appium.java_client.touch.WaitOptions.waitOptions;
 import static io.appium.java_client.touch.offset.PointOption.point;
 import static java.time.Duration.ofMillis;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.collections.map.HashedMap;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
 
-public class Test {
+public class TestClass extends BaseClass {
 
-	public static AndroidDriver driver = null;
-	public static WebDriverWait wait = null;
-	private DesiredCapabilities caps;
-
-	@BeforeMethod
-	public void beforeMethod() throws MalformedURLException {
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability("deviceName", "Android Emulator");
-		capabilities.setCapability("udid", "emulator-5554");
-		//capabilities.setCapability("deviceName", "Android Device");
-		//capabilities.setCapability("udid", "ce05171508dd082f01");
-		capabilities.setCapability("platformName", "Android");
-		// capabilities.setCapability("platformVersion", "9.0");
-		// String path =
-		// System.getProperty("user.dir")+"/app/com.amazon.mShop.android.shopping_2019-01-29.apk";
-		// capabilities.setCapability("app", path);
-		capabilities.setCapability("appPackage", "in.amazon.mShop.android.shopping");
-		capabilities.setCapability("appActivity", "com.amazon.mShop.home.HomeActivity");
-		capabilities.setCapability("newCommandTimeout", "100");
-		capabilities.setCapability("autoGrantPermissions", "true");
-		capabilities.setCapability("noReset", "false");
-		// capabilities.setCapability("unicodeKeyboard","false");
-		// capabilities.setCapability("resetKeyboard","false");
-
-		URL url = new URL("http://127.0.0.1:4723/wd/hub");
-		driver = new AndroidDriver(url, capabilities);
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		wait = new WebDriverWait(driver, 10);
-	}
-
-	@org.testng.annotations.Test
+	@Test
 	public void testScenario() {
 		try {
 			// Login Application
@@ -179,11 +140,7 @@ public class Test {
 		}
 	}
 
-	@AfterMethod
-	public void afterMethod() {
-		driver.closeApp();
-		driver.quit();
-	}
+	
 	
 	public void swipeUp(double an, double st, double en) {
 		Dimension size = driver.manage().window().getSize();
